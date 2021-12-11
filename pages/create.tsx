dd6ff66165ab7589ba7f2d40b50b24499b60510a
faisalsayed10 <faisal.sayed502@gmail.com>
2021-12-11 08:46:38 +0530

    Create and join session pages

--- pages/create.tsx
+++ pages/create.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import { useState } from "react";
 
 const Home: NextPage = () => {
-  const [sessionId, setSessionId] = useState("");
   const [sessionPassword, setSessionPassword] = useState("");
   const [username, setUsername] = useState("");
 
@@ -12,21 +11,13 @@ const Home: NextPage = () => {
     <Container>
       <Flex align="center" justify="center" flexDir="column" minH="100vh">
         <Text as="h2" fontSize="4xl" fontWeight="bold">
-          Join Session
+          Create Session
         </Text>
         <Input
           w="300px"
           variant="flushed"
           mb="4"
-          placeholder="Session ID"
           mt="8"
-          value={sessionId}
-          onChange={(e) => setSessionId(e.target.value)}
-        />
-        <Input
-          w="300px"
-          variant="flushed"
-          mb="4"
           placeholder="Session Password"
           value={sessionPassword}
           onChange={(e) => setSessionPassword(e.target.value)}
@@ -39,16 +30,13 @@ const Home: NextPage = () => {
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
-        <Button
-          w="300px"
-          disabled={!sessionId || !sessionPassword || !username}
-        >
-          Join!
+        <Button w="300px" disabled={!sessionPassword || !username}>
+          Create!
         </Button>
         <Text as="p" fontSize="sm" align="left" w="300px">
-          <Link href="/create">
+          <Link href="/">
             <chakra.span textDecor="underline" cursor="pointer">
-              Create a session
+              Join a session
             </chakra.span>
           </Link>{" "}
           instead
